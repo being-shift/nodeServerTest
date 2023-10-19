ba7e3d5f0b6623f1e9792793aedcacd9aeb7704d
being_shift <being_shift@icloud.com>
2023-10-19 14:44:57 +0900

    change blocking port 8080 to 8090

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 const express = require("express");
 const app = express();
 
-app.listen(8080, function(){
-	console.log("listening on 8080");
+app.listen(8090, function(){
+	console.log("listening on 8090");
 });
 
 // Controller Mapping
