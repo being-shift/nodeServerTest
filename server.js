8c5a7bb17a712b4f06904bbebc98ad0e044d3a3a
being_shift <being_shift@icloud.com>
2023-10-20 19:25:33 +0900

    change way to bootstrap

--- server.js
+++ server.js
@@ -2,16 +2,26 @@
 const express = require("express");
 const app = express();
 
-app.listen(8090, function(){
-	console.log("listening on 8090");
+app.listen(8080, function(){
+	console.log("listening on 8080");
 });
 
 // Controller Mapping
-app.get('/pet', function(req,res){
+app.get('', function(req,res){
 	//res.send('펫 용품을 구매할 수 있는 사이트 입니다.');
 	res.sendFile(__dirname + '/index.html');
-	console.log('어쩌구 저쩌구');
+	console.log('Main');
+});
+app.get('/pet', function(req,res){
+	//res.send('펫 용품을 구매할 수 있는 사이트 입니다.');
+	res.sendFile(__dirname + '/pet.html');
+	console.log('Pet shop');
 });
 
+app.get('/product', function(req,res){
+	//res.send('펫 용품을 구매할 수 있는 사이트 입니다.');
+	res.sendFile(__dirname + '/product.html');
+	console.log('물품구매');
+});
 // static directory 등록
 // app.use(express.static(__dirname + '폴더경로'));
